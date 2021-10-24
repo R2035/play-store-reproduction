e580d6438771adf328c28ce49d96bb373fc3d11f
R2035 <r2035.develop@gmail.com>
2021-10-24 16:03:40 +0900

    BottomNavigationViewによるFragmentの切り替えを実装

--- App/app/src/main/java/com/example/play/store/reproduction/screen/a01/MainActivity.kt
+++ App/app/src/main/java/com/example/play/store/reproduction/screen/a01/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.play.store.reproduction.screen.a01
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.play.store.reproduction.R
+import com.example.play.store.reproduction.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = (supportFragmentManager.findFragmentById(R.id.main_fragment_container) as NavHostFragment).navController
+        binding.mainBottomNavigationView.setupWithNavController(navController)
     }
 }
